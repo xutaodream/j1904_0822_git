6207c844017daf3734fc9cebd271275a58956dcf
xutaodream <1611691236@qq.com>
2019-08-22 15:24:14 +0800

    123

--- src/main/java/com/qf/j1904/pojo/user.java
+++ src/main/java/com/qf/j1904/pojo/user.java
@@ -3,7 +3,9 @@ package com.qf.j1904.pojo;
 public class user {
     public static void main(String[] args) {
 
+        String ss = "123";
         System.out.println("args = ");
+        System.out.println("ss = " + ss);
     }
 
 }
